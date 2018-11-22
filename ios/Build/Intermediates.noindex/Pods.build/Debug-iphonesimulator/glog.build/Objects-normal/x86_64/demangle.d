d650e812b5c8d4cf76bc873cafc507deded13147
Leslie Sibanda <lesliesibanda123@gmail.com>
2018-11-22 00:15:13 +0000

    expo icon and splash configured

---
+++ ios/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/glog.build/Objects-normal/x86_64/demangle.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/programmer/Desktop/Mobile/Weft/ios/Pods/glog/src/demangle.cc \
+  /Users/programmer/Desktop/Mobile/Weft/ios/Pods/Target\ Support\ Files/glog/glog-prefix.pch \
+  /Users/programmer/Desktop/Mobile/Weft/ios/Pods/glog/src/demangle.h \
+  /Users/programmer/Desktop/Mobile/Weft/ios/Pods/glog/src/config.h \
+  /Users/programmer/Desktop/Mobile/Weft/ios/Pods/glog/src/glog/logging.h \
+  /Users/programmer/Desktop/Mobile/Weft/ios/Pods/Headers/Private/glog/glog/log_severity.h \
+  /Users/programmer/Desktop/Mobile/Weft/ios/Pods/Headers/Private/glog/glog/vlog_is_on.h
