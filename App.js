38e7ce6cf5dfcc2247065622f9d8e5ed49ad1898
Leslie Sibanda <lesliesibanda123@gmail.com>
2018-11-25 00:59:31 +0000

    Frame images added to project and the async video lodas straight after the loading

--- App.js
+++ App.js
@@ -1,16 +1,74 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Constants } from 'expo';
+import { Constants, Video, Asset, AppLoading } from 'expo';
+import Hair from "./hair.mp4";
 
 export default class App extends React.Component {
-  render() {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        path: null,
+      };
+    }
+  state = {
+      isReady: false,
+    };
+    async _cacheResourcesAsync() {
+       const images = [
+         require('./hair.mp4'),
+       ];
+
+       const cacheImages = images.map((image) => {
+         return Asset.fromModule(image).downloadAsync();
+       });
+       return Promise.all(cacheImages)
+
+     }
+
+
+    render() {
+       if (!this.state.isReady) {
+         return (
+           <AppLoading
+             startAsync={this._cacheResourcesAsync}
+             onFinish={() => this.setState({ isReady: true })}
+             onError={console.warn}
+           />
+         );
+       };
+
     return (
       <View style={styles.container}>
-        <Text>{Constants.manifest.version}</Text>
+      <Video
+      source={Hair}   // Can be a URL or a local file.
+      rate={2.0}
+   volume={1.0}
+   isMuted={false}
+   resizeMode="cover"
+   shouldPlay
+   isLooping
+   style={StyleSheet.absoluteFill}
+       />
+      </View>
+      );
+        {this.state.path ? this.renderVideo() : this.renderImage()}
+}
+
+renderImage() {
+    return (
+      <View style="styles.container">
+        <Text>
+        Cancel
+        </Text>
       </View>
     );
   }
+
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,5 +76,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
